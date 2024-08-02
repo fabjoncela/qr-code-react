c5ab0d670fad7ae9e39eba8a2901f340963ee78f
fabjoncela <fabjoncela@gmail.com>
2024-08-02 15:51:38 +0200

    added dwn button, redesign

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-// src/App.js
-import React, { useState } from 'react';
-import QRCode from 'qrcode.react';
+import React, { useState, useEffect } from 'react';
+import QRCodeLib from 'qrcode'; // Import the qrcode library
+import QRCode from 'qrcode.react'; // Import QRCode component
 import './App.css';
 
 function App() {
   const [url, setUrl] = useState('');
+  const [qrCodeUrl, setQrCodeUrl] = useState('');
+
+  useEffect(() => {
+    if (url) {
+      QRCodeLib.toDataURL(url)
+        .then((dataUrl) => {
+          setQrCodeUrl(dataUrl);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      setQrCodeUrl('');
+    }
+  }, [url]);
 
   const handleInputChange = (e) => {
     setUrl(e.target.value);
+  };
+
+  const handleDownload = () => {
+    if (qrCodeUrl) {
+      const sanitizedFilename = url
+        .replace(/[^a-zA-Z0-9]/g, '_') // Replace non-alphanumeric characters with underscores
+        .slice(0, 50); // Limit filename length to 50 characters
+      const link = document.createElement('a');
+      link.href = qrCodeUrl;
+      link.download = `${sanitizedFilename}.png`; // Use sanitized URL as filename
+      link.click();
+    }
   };
 
   return (
@@ -19,10 +46,13 @@ function App() {
         value={url}
         onChange={handleInputChange}
       />
-      {url && (
+      {qrCodeUrl && (
         <div>
           <h2>QR Code:</h2>
-          <QRCode value={url} />
+          <QRCode value={url} size={256} /> {/* Adjusted size */}
+          <div>
+            <button onClick={handleDownload} className="download-button">Download QR Code</button>
+          </div>
         </div>
       )}
     </div>
